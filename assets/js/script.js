46a14aeca8f8485ff31d14f9f3dd6b6d2cc0ed4d
Isaac E. Louzeiro <isaacelias1110@gmail.com>
2022-02-24 20:15:27 -0300

    relogio parcialmente pronto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,17 @@
-
 setInterval(() => {
     const d = new Date();
-    let time = d.getTime();
     const h = d.getHours();
     const m = d.getMinutes();
     const s = d.getSeconds();
-    document.getElementById('hour').innerHTML = `${h +':'+m+':'+s}`;
+
+    // digital time
+    const hourDegrees = (h / 12) * 360 + (m / 60) * 30 + 90;
+    const minsDegrees = (m / 60) * 360 + (s / 60) * 6 + 90;
+    const secondsDegrees = (s / 60) * 360 + 90;
+    document.getElementById('hour').innerHTML = `${`${h}:${m}:${s}`}`;
+
+    // analog time
+    document.getElementById('dashHour').style.transform = `rotate(${hourDegrees}deg)`;
+    document.getElementById('dashMinutes').style.transform = `rotate(${minsDegrees}deg)`;
+    document.getElementById('dashSeconds').style.transform = `rotate(${secondsDegrees}deg)`;
 }, 1000);
