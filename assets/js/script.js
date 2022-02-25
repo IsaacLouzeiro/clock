cb3b20678348266e2e0d63c944017542b8729a65
Isaac E. Louzeiro <isaacelias1110@gmail.com>
2022-02-25 16:31:59 -0300

    alterando variaveis

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,18 @@
 setInterval(() => {
+    // get Time
     const d = new Date();
     const h = d.getHours();
     const m = d.getMinutes();
     const s = d.getSeconds();
 
     // digital time
-    const hourDegrees = (h / 12) * 360 + (m / 60) * 30 + 90;
-    const minsDegrees = (m / 60) * 360 + (s / 60) * 6 + 90;
-    const secondsDegrees = (s / 60) * 360 + 90;
+    const hDeg = (h / 12) * 360 + (m / 60) * 30 + 90;
+    const mDeg = (m / 60) * 360 + (s / 60) * 6 + 90;
+    const sDeg = (s / 60) * 360 + 90;
     document.getElementById('hour').innerHTML = `${`${h}:${m}:${s}`}`;
 
     // analog time
-    document.getElementById('dashHour').style.transform = `rotate(${hourDegrees}deg)`;
-    document.getElementById('dashMinutes').style.transform = `rotate(${minsDegrees}deg)`;
-    document.getElementById('dashSeconds').style.transform = `rotate(${secondsDegrees}deg)`;
+    document.getElementById('dashHour').style.transform = `rotate(${hDeg}deg)`;
+    document.getElementById('dashMinutes').style.transform = `rotate(${mDeg}deg)`;
+    document.getElementById('dashSeconds').style.transform = `rotate(${sDeg}deg)`;
 }, 1000);
